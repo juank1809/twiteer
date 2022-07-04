9102b930ab3f8a094fa63e564f267dc0800dea8f
Juan García <juank1809@gmail.com>
2022-07-04 16:38:54 -0500

    refactor so we can use MVC pattern correctly

--- packages/mock-service/src/app.ts
+++ packages/mock-service/src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import router from "./routes/tweets";
+import tweetController from "./controllers/tweets.controller";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use("/", router);
+app.use("/", tweetController);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
