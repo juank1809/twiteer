a9967ff9fd488bb1239be64725474c3b07c3814c
Juan García <juank1809@gmail.com>
2022-07-12 20:38:53 -0500

    make TweetTextArea a more complete and specific component

--- packages/twitter-ui/src/components/TweetTextArea/TweetTextArea.tsx
+++ packages/twitter-ui/src/components/TweetTextArea/TweetTextArea.tsx
@@ -1,74 +1,38 @@
 import React from "react";
-import getUserInitials from "../../utils/getUserInitials";
-import { useState } from "react";
-import { useTweets } from "../../hooks/useTweets";
-import { UserImage } from "../UserImage";
+import TextareaAutosize from "react-textarea-autosize";
 
-const newTweetMarkup = {
-  user: {
-    fullName: "Christopher Francisco",
-    username: "@christopher",
-  },
-  favoriteCount: 0,
-  retweetCount: 0,
-  replyCount: 0,
-  type: "default",
-} as const;
+interface TweetTextAreaProps {
+  name: string;
+  label: string;
+  handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  value: string;
+  minRows?: number;
+}
 
-const TweetTextArea: React.FC = () => {
-  const [tweetText, setTweetText] = useState("");
-  const { tweets, addTweet } = useTweets();
-
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setTweetText(e.target.value);
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    addTweet({
-      ...newTweetMarkup,
-      id: tweets.length + 1,
-      message: tweetText,
-    });
-    cleanAfterTweet();
-  };
-
-  const cleanAfterTweet = () => {
-    setTweetText("");
-  };
-
+const TweetTextArea: React.FC<TweetTextAreaProps> = ({
+  name,
+  label,
+  handleChange,
+  value,
+  minRows = 3,
+}) => {
   return (
-    <div className="tweet-text-area">
-      <div className="tweet-text-area__main-container">
-        <UserImage size="medium">
-          {" "}
-          {getUserInitials(newTweetMarkup.user.fullName)}{" "}
-        </UserImage>
-        <form
-          className="tweet-text-area__form-container"
-          onSubmit={handleSubmit}
-        >
-          <textarea
-            className="tweet-text-area__text-box"
-            name="tweet-text-area"
-            value={tweetText}
-            onChange={handleChange}
-            id="tweet-text-area"
-            required
-            rows={3}
-            cols={40}
-          />
-          <label
-            className="tweet-text-area__text-box__label"
-            htmlFor="tweet-text-area"
-          >
-            What are you thinking today?
-          </label>
-          <button type="submit" className="primary-button">
-            Tweet
-          </button>
-        </form>
-      </div>
+    <div className="text-box__container">
+      <TextareaAutosize
+        id={name}
+        minRows={minRows}
+        maxRows={4}
+        className="text-box"
+        name={name}
+        value={value}
+        onChange={handleChange}
+        required
+        rows={3}
+        cols={40}
+      />
+      <label className="text-box__label" htmlFor={name}>
+        {label}
+      </label>
     </div>
   );
 };
