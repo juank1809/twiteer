002671a8b2591aaa0970733ea9ffc4454f84d449
Juan García <juank1809@gmail.com>
2022-10-07 13:31:37 -0500

    create a new endpoint to retweet a tweet

--- packages/mock-service/src/controllers/tweets.controller.ts
+++ packages/mock-service/src/controllers/tweets.controller.ts
@@ -12,4 +12,19 @@ router.post("/tweets", (req: Request, res: Response, _: NextFunction) => {
   res.send(tweetsData);
 });
 
+router.post("retweet", (req: Request, res: Response) => {
+  const tweetId = req.body.tweet.id;
+  const toRetweet = tweetsData.find((tweet) => tweet.id === tweetId);
+
+  if (!toRetweet) {
+    return res.send({
+      error: "Tweet to retweet not found!",
+    });
+  }
+
+  tweetsData.push(toRetweet);
+
+  res.send(tweetsData);
+});
+
 export default router;
