331bd22d71a01c9800b4337613cfb3dbe61486ee
Juan García <82288753+juank1809@users.noreply.github.com>
2022-10-10 13:29:39 -0500

    Revert "Create a new endpoint to retweet a tweet (#52)"
    This reverts commit 7d735610198a66cb2d3d1d30c29abc951060e0fe.

--- packages/mock-service/tweetsData.ts
+++ packages/mock-service/tweetsData.ts
@@ -1,35 +1,32 @@
 export const tweetsData = [
-  {
-    id: 1,
-    user: {
-      fullName: "Christoper Francisco",
-      username: "christopher",
+    {
+      user: {
+        fullName: "Christoper Francisco",
+        username: "christopher",
+      },
+      message: "I like coding",
+      favoriteCount: 3,
+      replyCount: 2,
+      retweetCount: 1,
     },
-    message: "I like coding",
-    favoriteCount: 3,
-    replyCount: 2,
-    retweetCount: 1,
-  },
-  {
-    id: 2,
-    user: {
-      fullName: "Jaun García",
-      username: "juank",
+    {
+      user: {
+        fullName: "Jaun García",
+        username: "juank",
+      },
+      message: "I like coding",
+      favoriteCount: 3,
+      replyCount: 2,
+      retweetCount: 1,
     },
-    message: "I like coding",
-    favoriteCount: 3,
-    replyCount: 2,
-    retweetCount: 1,
-  },
-  {
-    id: 3,
-    user: {
-      fullName: "Daniel Fernando",
-      username: "dani",
+    {
+      user: {
+        fullName: "Daniel Fernando",
+        username: "dani",
+      },
+      message: "I like coding",
+      favoriteCount: 3,
+      replyCount: 2,
+      retweetCount: 1,
     },
-    message: "I like coding",
-    favoriteCount: 3,
-    replyCount: 2,
-    retweetCount: 1,
-  },
-];
+  ];
