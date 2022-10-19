ca4c41364d846c7bc953231b013e2c8828fd63e8
Juan García <82288753+juank1809@users.noreply.github.com>
2022-10-19 16:42:08 -0500

    chore: create E2E test for favouriting a tweet (#62)
    * chore: create E2E test for favouriting a tweet
    
    * Delete home_page.cy.ts
    
    * Delete index.d.ts
    
    Co-authored-by: Juan García <juank1809@gmail.com>

--- packages/twitter-ui/cypress.config.js
+++ packages/twitter-ui/cypress.config.js
@@ -1,7 +1,7 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-};
+    baseUrl: 'http://localhost:3000'
+  }
+})
