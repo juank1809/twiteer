c89ecf169180112a674f197d58cd3272e9f43fc0
Juan García <82288753+juank1809@users.noreply.github.com>
2022-10-19 16:41:52 -0500

    Delete index.d.ts

--- packages/twitter-ui/cypress/support/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Cypress {
-  interface Chainable {
-    /**
-     * Custom command to select the first tweet by the testdata-id HTML attribute
-     * @example cy.dataCy('greeting')
-     */
-    getFirstTweet(): Chainable<Element>;
-  }
-}
