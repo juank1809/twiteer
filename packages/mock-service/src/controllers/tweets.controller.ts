d2ab853a1b88777fc4f217a94eefccdf40ce84f5
Juan García <juank1809@gmail.com>
2022-10-30 21:27:25 -0500

    feat: create endpoint to alter data

--- packages/mock-service/src/controllers/tweets.controller.ts
+++ packages/mock-service/src/controllers/tweets.controller.ts
@@ -1,7 +1,47 @@
 import express, { Request, Response, NextFunction } from "express";
-import { tweetsData } from "../../tweetsData";
+import {
+  tweetsData,
+  getGetRouteTweetsData,
+  getPostRouteTweetsData,
+  getRetweetRouteTweetsData,
+  getFavoriteRouteTweetsData,
+  setGetRouteTweetsData,
+  setPostRouteTweetsData,
+  setFavoriteRouteTweetsData,
+  setRetweetRouteTweetsData,
+  clearTweetsDataProvider,
+  isTweetsDataProviderEmpty,
+} from "../tweets-data-provider";
 
 const router = express.Router();
+
+/* Middleware intercepter util for the server-state route */
+router.use("/", (req: Request, res: Response, next: NextFunction) => {
+  if (!isTweetsDataProviderEmpty()) next();
+
+  /*/ If the incoming request is a GET request and there exists data in the 
+  GETRouteTweetsDataProvider the return the stored data.
+  */
+  if (req.method === "GET" && getGetRouteTweetsData()) {
+    console.log("hello", getGetRouteTweetsData());
+    return res.json(getGetRouteTweetsData());
+  } else if (
+    req.method === "POST" &&
+    req.baseUrl === "/tweets" &&
+    getGetRouteTweetsData()
+  ) {
+    return res.send(getPostRouteTweetsData());
+  } else if (
+    req.method === "POST" &&
+    req.baseUrl === "/retweet" &&
+    getRetweetRouteTweetsData()
+  ) {
+    return res.send(getRetweetRouteTweetsData());
+  } else if (req.method === "PUT" && getFavoriteRouteTweetsData()) {
+    return res.send(getFavoriteRouteTweetsData());
+  }
+  next();
+});
 
 router.get("/tweets", (req: Request, res: Response, _: NextFunction) => {
   res.send(tweetsData);
@@ -40,6 +80,32 @@ router.post("favorite", (req: Request, res: Response) => {
   });
 
   return res.send(tweetsData);
+});
+
+/* This helper route allow us to change different route data methods 
+using the middleware defined above of this module */
+router.put("/server-state", (req: Request, res: Response) => {
+  const { action, value } = req.body;
+  console.log(value);
+  switch (action) {
+    case "get.tweets":
+      console.log("is this being run");
+      setGetRouteTweetsData(value);
+      return res.send(getGetRouteTweetsData());
+      break;
+    case "post.tweets":
+      setPostRouteTweetsData(value);
+      break;
+    case "favorite.tweets":
+      setFavoriteRouteTweetsData(value);
+      break;
+    case "retweet.tweets":
+      setRetweetRouteTweetsData(value);
+      break;
+    case "clear.tweets":
+      clearTweetsDataProvider();
+      break;
+  }
 });
 
 export default router;
