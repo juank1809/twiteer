367e82f15b1739d8a6aeeb516d17a6cdde6a5210
Juan García <82288753+juank1809@users.noreply.github.com>
2023-04-04 14:53:29 -0500

    Revert "Testing" (#94)
    Revert "Testing (#93)"
    
    This reverts commit 9a89f00e67af9adedf269dad9f79b9bd30e6fca8.

--- packages/twitter-ui/jest.config.js
+++ packages/twitter-ui/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
+
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.ts?$': 'ts-jest',
+    },
+
 }
-
-
