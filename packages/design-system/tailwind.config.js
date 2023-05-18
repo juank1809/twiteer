3a6494e61e454a63dc1cba77f9e26b531d33604a
Juan García <juank1809@gmail.com>
2023-05-17 19:48:15 -0500

    feat(twiteer-ui): add overall layout

---
+++ packages/design-system/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'primary': '#1DA1F2',
+        'black': '#000000',
+        'gray': '#8899A6',
+        'black-gray': '#2F3336',
+        'on-hover-color': "rgba(255, 255, 255, 0.03)"
+      },
+      boxShadow: {
+        'dropdown-shadow': 'rgba(255, 255, 255, 0.2) 0px 0px 15px, rgba(255, 255, 255, 0.15) 0px 0px 3px 1px'
+      }
+    },
+  },
+  plugins: [],
+}
